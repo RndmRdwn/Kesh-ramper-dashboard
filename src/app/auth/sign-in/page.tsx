56d19bbc0830ab9a5ce7738c1a5bbff501be07a8
Redwan Mo <redwanmuhiadin@outlook.com>
2024-11-21 18:47:59 +0300

    sign-up process

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 const SignInPage = () => {
     return (
         <>
-            <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+            <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
                 <Link
                     href="/auth/sign-up"
                     className={cn(
--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from 'next';
-import Image from 'next/image';
 import Link from 'next/link';
 import SignUpForm from './components/sign-up-form';
 import { buttonVariants } from '@/components/shadcn/ui/button';
@@ -13,18 +12,32 @@ export const metadata: Metadata = {
 const SignInPage = () => {
     return (
         <>
-            <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+            <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
                 <Link
                     href="/auth/sign-in"
                     className={cn(
                         buttonVariants({ variant: 'ghost' }),
-                        'absolute right-4 top-4 md:right-8 md:top-8'
+                        'absolute left-4 top-4 md:left-8  md:top-8'
                     )}
                 >
                     Sign in
                 </Link>
+                <div className="lg:p-8">
+                    <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[450px]">
+                        <div className="flex flex-col space-y-2 text-center">
+                            <h1 className="text-2xl font-semibold tracking-tight">
+                                Create an account
+                            </h1>
+                            <p className="text-sm text-muted-foreground">
+                                Create an account to get started
+                            </p>
+                        </div>
+                        <SignUpForm />
+                       
+                    </div>
+                </div>
                 <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-                    <div className="absolute inset-0 bg-black" />
+                    <div className="absolute inset-0 bg-gradient-to-tr from-primary/70" />
                     <div className="relative z-20 flex items-center text-lg font-medium">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -40,16 +53,6 @@ const SignInPage = () => {
                         </svg>
                         easy-ramp
                     </div>
-
-                    <div className="z-50 flex items-center justify-center h-full">
-                        <Image
-                            src="/images/img-2.png"
-                            width={500}
-                            height={500}
-                            alt="sign-in image"
-                        />
-                    </div>
-
                     <div className="relative z-20 mt-auto">
                         <blockquote className="space-y-2">
                             <p className="text-lg">
@@ -59,36 +62,6 @@ const SignInPage = () => {
                             </p>
                             <footer className="text-sm">Sofia Davis</footer>
                         </blockquote>
-                    </div>
-                </div>
-                <div className="lg:p-8">
-                    <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[450px]">
-                        <div className="flex flex-col space-y-2 text-center">
-                            <h1 className="text-2xl font-semibold tracking-tight">
-                                Create an account
-                            </h1>
-                            <p className="text-sm text-muted-foreground">
-                                Create an account to get started
-                            </p>
-                        </div>
-                        <SignUpForm />
-                        <p className="px-8 text-center text-sm text-muted-foreground">
-                            By clicking continue, you agree to our{' '}
-                            <Link
-                                href="/terms"
-                                className="underline underline-offset-4 hover:text-primary"
-                            >
-                                Terms of Service
-                            </Link>{' '}
-                            and{' '}
-                            <Link
-                                href="/privacy"
-                                className="underline underline-offset-4 hover:text-primary"
-                            >
-                                Privacy Policy
-                            </Link>
-                            .
-                        </p>
                     </div>
                 </div>
             </div>
